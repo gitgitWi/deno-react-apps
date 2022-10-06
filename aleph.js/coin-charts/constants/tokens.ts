35a176fa038bcd7f1f7644f8e6ae3a39aa59b969
gitgitWi <wiii@kakao.com>
2022-10-07 00:18:55 +0900

    feat(coin/constants) aws tokens 상수 추가

--- aleph.js/coin-charts/constants/tokens.ts
+++ aleph.js/coin-charts/constants/tokens.ts
@@ -1,4 +1,9 @@
 export const POLYGON_API = {
   baseUrl: `https://api.polygon.io`,
-  apiToken: Deno.env.get('POLYGON_IO_API_KEY'),
+  apiToken: Deno.env.get('POLYGON_IO_API_KEY') || '',
+} as const;
+
+export const AWS = {
+  accessKeyId: Deno.env.get('AWS_ACCESS_KEY_ID') || '',
+  secretAccessKey: Deno.env.get('AWS_SECRET_ACCESS_KEY') || '',
 } as const;
