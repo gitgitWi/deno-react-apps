8aac4bfe73e9ed87482a21f6750ac67e656ed137
gitgitWi <wiii@kakao.com>
2022-10-07 00:20:02 +0900

    feat(coin/dynamodb) connectDB, insertMany 메서드 구현

--- aleph.js/coin-charts/services/db/dynamodb/DynamoDBAdapter.ts
+++ aleph.js/coin-charts/services/db/dynamodb/DynamoDBAdapter.ts
@@ -1,19 +1,67 @@
-import { DynamoDBClient, GetItemCommand, PutItemCommand } from '@aws-sdk/client-dynamodb';
+import {
+  DynamoDBClient,
+  BatchWriteItemCommand,
+  AttributeValue,
+} from '@aws-sdk/client-dynamodb';
 
+import { AWS } from '@/constants/mod.ts';
 import { DBClientAdapter } from '@/services/db/DBClientAdapter.ts';
 
 import { TableNames } from './constants.ts';
 
 /**
  * @see https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/index.html
+ * @see https://docs.aws.amazon.com/ko_kr/amazondynamodb/latest/developerguide/GettingStarted.html
  */
 export class DynamoDBAdapter extends DBClientAdapter {
+  private client!: DynamoDBClient;
+
   constructor() {
     super();
+    this.connectDB();
+  }
+
+  connectDB() {
+    this.client = new DynamoDBClient({
+      region: 'ap-northeast-2',
+      credentials: {
+        accessKeyId: AWS.accessKeyId,
+        secretAccessKey: AWS.secretAccessKey,
+      },
+    });
   }
 
   insert<T>(insertItem: T): T {
+    console.log(insertItem);
     return insertItem;
+  }
+
+  /**
+   * @see https://docs.aws.amazon.com/ko_kr/amazondynamodb/latest/developerguide/ServiceQuotas.html#limits-api
+   * - batch는 한번에 25개까지만 가능
+   */
+  insertMany<T extends Record<string, AttributeValue>>(
+    tableName: TableNames,
+    items: T[],
+  ) {
+    try {
+      const result = this.client.send(
+        new BatchWriteItemCommand({
+          RequestItems: {
+            [tableName]: items.map((item) => ({
+              PutRequest: {
+                Item: item,
+              },
+            })),
+          },
+        }),
+      );
+      console.log('DynamoDBAdapter$$insertMany$$result\n', result);
+      return result;
+    } catch (error) {
+      console.error(`DynamoDBAdapter$$insertMany$$ERROR\n`, error);
+      return undefined;
+    }
   }
 
   // getOne<T>(...args: any[]): T {
